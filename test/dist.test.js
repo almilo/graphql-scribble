ffd66aabd4fc5211461ea1953fbe3a1f0f625139
Alberto Mijares <almilo@almilo.es>
2017-11-13 23:28:38 +0100

    test(integration): improve integration test

--- test/dist.test.js
+++ test/dist.test.js
@@ -1,24 +1,47 @@
 import { createSchemaAnnotationExtractor } from '..';
 
 describe('lib tests', () => {
-    const schemaAnnotationExtractor = createSchemaAnnotationExtractor({
-        foo: {},
-        bar: {}
-    });
-    const schema = `
-        type User @foo(bar: "barValue", baz: "bazValue") {
-            chirps: [Chirp]
-        }
-    `;
-
     it(`should extract all annotation data when the schema is annotated`, () => {
+        const schemaAnnotationExtractor = createSchemaAnnotationExtractor({
+            foo: {
+                schema: {
+                    bar: {
+                        type: 'string',
+                        required: true
+                    },
+                    baz: {
+                        type: 'boolean'
+                    }
+                }
+            },
+            bar: {
+                schema: {
+                    value: {
+                        type: 'number',
+                        required: true
+                    }
+                }
+            }
+        });
+        const schema = `
+            type User @foo(bar: "barValue", baz: false) {
+                chirps: [Chirp] @bar(value: 42)
+            }
+        `;
+
         const annotationData = schemaAnnotationExtractor.extract(schema);
 
         expect(annotationData).toEqual({
                 foo: [
                     {
                         node: jasmine.any(Object),
-                        arguments: {"bar": "barValue", "baz": "bazValue"}
+                        arguments: {bar: "barValue", baz: false}
+                    }
+                ],
+                bar: [
+                    {
+                        node: jasmine.any(Object),
+                        arguments: {value: 42}
                     }
                 ]
             }
